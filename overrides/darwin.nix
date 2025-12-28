5320466f3386660712c8bdab22c6f95b795ef82f
Andrey Stepantsov <andrey.stepantsov@nokia.com>
2025-12-28 12:58:18 -0800

    wip: auto-commit for CI

---
+++ overrides/darwin.nix
@@ -0,0 +1,87 @@
+{ pkgs, googleFix, unstable }:
+final: prev:
+{
+  # ... Google overrides ...
+  google-cloud-aiplatform = prev.google-cloud-aiplatform.overridePythonAttrs googleFix;
+  google-cloud-storage = prev.google-cloud-storage.overridePythonAttrs googleFix;
+  google-cloud-core = prev.google-cloud-core.overridePythonAttrs googleFix;
+  google-api-core = prev.google-api-core.overridePythonAttrs googleFix;
+  google-resumable-media = prev.google-resumable-media.overridePythonAttrs googleFix;
+  google-crc32c = prev.google-crc32c.overridePythonAttrs googleFix;
+  google-cloud-resource-manager = prev.google-cloud-resource-manager.overridePythonAttrs googleFix;
+  google-cloud-bigquery = prev.google-cloud-bigquery.overridePythonAttrs googleFix;
+
+  meson = unstable.meson;
+  ninja = unstable.ninja;
+
+  scipy = prev.scipy.overridePythonAttrs (old: {
+    nativeBuildInputs = (pkgs.lib.filter 
+      (p: (p.pname or "") != "meson" && (p.pname or "") != "ninja") 
+      (old.nativeBuildInputs or [])) 
+    ++ [
+      unstable.meson 
+      unstable.ninja
+      unstable.pkg-config
+      unstable.gfortran
+    ] ++ [ pkgs.darwin.apple_sdk.frameworks.Accelerate ];
+    
+    buildInputs = (old.buildInputs or []) ++ [ unstable.gfortran.cc.lib ];
+    
+    # FIX: Use DYLD_LIBRARY_PATH to help the test executable find libgfortran
+    preConfigure = (old.preConfigure or "") + ''
+      export FC=${unstable.gfortran}/bin/gfortran
+      export DYLD_LIBRARY_PATH="${unstable.gfortran.cc.lib}/lib:$DYLD_LIBRARY_PATH"
+      export LDFLAGS="-L${unstable.gfortran.cc.lib}/lib -Wl,-rpath,${unstable.gfortran.cc.lib}/lib $LDFLAGS"
+    '';
+
+    preferWheel = true;
+    configurePhase = "true"; 
+  });
+
+  # Rpds-py override for Darwin (unchanged, it works)
+  rpds-py = prev.rpds-py.overridePythonAttrs (old: 
+    let
+      rustDeps = unstable.rustPlatform.fetchCargoVendor {
+        inherit (final.rpds-py) src;
+        name = "rpds-py-vendor";
+        hash = "sha256-2skrDC80g0EKvTEeBI4t4LD7ZXb6jp2Gw+owKFrkZzc=";
+      };
+    in {
+      preferWheel = false; 
+      src = pkgs.fetchPypi {
+        pname = "rpds_py";
+        version = "0.22.3";
+        hash = "sha256-4y/uirRdPC222hmlMjvDNiI3yLZTxwGUQUuJL9BqCA0=";
+      };
+      srcCargoDeps = rustDeps;
+      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [
+        unstable.cargo unstable.rustc unstable.maturin pkgs.python311Packages.pip pkgs.pkg-config
+        pkgs.libiconv pkgs.darwin.apple_sdk.frameworks.Security pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+      ];
+      buildInputs = (old.buildInputs or []) ++ [ pkgs.libiconv ];
+      preConfigure = ''
+         mkdir -p .cargo
+         cat > .cargo/config.toml <<EOF
+         [source.crates-io]
+         replace-with = "vendored-sources"
+         [source.vendored-sources]
+         directory = "$srcCargoDeps"
+         EOF
+         export CARGO_HOME=$(pwd)/.cargo
+         export RUSTFLAGS="-L ${pkgs.libiconv}/lib -l iconv"
+      '';
+      buildPhase = ''
+        export PATH="${unstable.cargo}/bin:${unstable.rustc}/bin:$PATH"
+        maturin build --release --jobs $NIX_BUILD_CORES --strip -i python3
+      '';
+      installPhase = ''
+        mkdir -p $out
+        wheel=$(find target/wheels -name "*.whl" | head -n 1)
+        pip install --no-deps --prefix=$out "$wheel"
+        mkdir -p dist && cp "$wheel" dist/
+      '';
+      wheelUnpackPhase = "true"; 
+  });
+  
+  watchfiles = prev.watchfiles.overridePythonAttrs (old: { preferWheel = true; });
+}
---
+++ overrides/linux.nix
@@ -0,0 +1,102 @@
+{ pkgs, googleFix, unstable }:
+final: prev:
+let
+  cleanMesonBinary = unstable.meson.overrideAttrs (old: { setupHook = null; });
+  cleanNinjaBinary = unstable.ninja.overrideAttrs (old: { setupHook = null; });
+in {
+  # ... Google overrides ...
+  google-cloud-aiplatform = prev.google-cloud-aiplatform.overridePythonAttrs googleFix;
+  google-cloud-storage = prev.google-cloud-storage.overridePythonAttrs googleFix;
+  google-cloud-core = prev.google-cloud-core.overridePythonAttrs googleFix;
+  google-api-core = prev.google-api-core.overridePythonAttrs googleFix;
+  google-resumable-media = prev.google-resumable-media.overridePythonAttrs googleFix;
+  google-crc32c = prev.google-crc32c.overridePythonAttrs googleFix;
+  google-cloud-resource-manager = prev.google-cloud-resource-manager.overridePythonAttrs googleFix;
+  google-cloud-bigquery = prev.google-cloud-bigquery.overridePythonAttrs googleFix;
+
+  meson = pkgs.python311Packages.buildPythonPackage {
+    pname = "meson";
+    version = unstable.meson.version;
+    format = "other";
+    src = ./.; 
+    unpackPhase = "true";
+    installPhase = ''
+      mkdir -p $out/bin
+      ln -s ${cleanMesonBinary}/bin/meson $out/bin/meson
+      site_packages=$out/lib/python3.11/site-packages
+      mkdir -p $site_packages/meson-${unstable.meson.version}.dist-info
+      echo "Metadata-Version: 2.1" > $site_packages/meson-${unstable.meson.version}.dist-info/METADATA
+      echo "Name: meson" >> $site_packages/meson-${unstable.meson.version}.dist-info/METADATA
+      echo "Version: ${unstable.meson.version}" >> $site_packages/meson-${unstable.meson.version}.dist-info/METADATA
+      ln -s ${cleanMesonBinary}/lib/python*/site-packages/mesonbuild $site_packages/mesonbuild
+    '';
+    propagatedBuildInputs = [ cleanMesonBinary ];
+  };
+
+  ninja = pkgs.python311Packages.buildPythonPackage {
+    pname = "ninja";
+    version = unstable.ninja.version;
+    format = "other";
+    src = ./.; 
+    unpackPhase = "true";
+    installPhase = ''
+      mkdir -p $out/bin
+      ln -s ${cleanNinjaBinary}/bin/ninja $out/bin/ninja
+      site_packages=$out/lib/python3.11/site-packages
+      mkdir -p $site_packages/ninja-${unstable.ninja.version}.dist-info
+      echo "Metadata-Version: 2.1" > $site_packages/ninja-${unstable.ninja.version}.dist-info/METADATA
+      echo "Name: ninja" >> $site_packages/ninja-${unstable.ninja.version}.dist-info/METADATA
+      echo "Version: ${unstable.ninja.version}" >> $site_packages/ninja-${unstable.ninja.version}.dist-info/METADATA
+    '';
+    propagatedBuildInputs = [ cleanNinjaBinary ];
+  };
+
+  meson-python = prev.meson-python.overridePythonAttrs (old: {
+    nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ final.meson ];
+    propagatedBuildInputs = (old.propagatedBuildInputs or []) ++ [ final.meson ];
+  });
+
+  pybind11 = prev.pybind11.overridePythonAttrs (old: {
+    nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ final.ninja ];
+  });
+
+  scipy = prev.scipy.overridePythonAttrs (old: {
+    nativeBuildInputs = (pkgs.lib.filter 
+      (p: (p.pname or "") != "meson" && (p.pname or "") != "ninja") 
+      (old.nativeBuildInputs or [])) 
+    ++ [ final.meson final.ninja unstable.pkg-config unstable.gfortran ];
+    preferWheel = true;
+    configurePhase = "true";
+  });
+
+  watchfiles = prev.watchfiles.overridePythonAttrs (old: {
+    preferWheel = true;
+    propagatedBuildInputs = (pkgs.lib.filter (p: p.pname != "anyio") old.propagatedBuildInputs) ++ [ final.anyio ];
+  });
+
+  # RPDS-PY Linux Fix: Use standard hooks with pyproject format
+  rpds-py = prev.rpds-py.overridePythonAttrs (old: 
+    let
+      rustDeps = unstable.rustPlatform.fetchCargoVendor {
+        inherit (final.rpds-py) src;
+        name = "rpds-py-vendor";
+        hash = "sha256-2skrDC80g0EKvTEeBI4t4LD7ZXb6jp2Gw+owKFrkZzc=";
+      };
+    in {
+      preferWheel = false; 
+      format = "pyproject";
+      src = pkgs.fetchPypi {
+        pname = "rpds_py";
+        version = "0.22.3";
+        hash = "sha256-4y/uirRdPC222hmlMjvDNiI3yLZTxwGUQUuJL9BqCA0=";
+      };
+      cargoDeps = rustDeps;
+      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [
+        unstable.cargo unstable.rustc unstable.maturin unstable.rustPlatform.maturinBuildHook pkgs.pkg-config
+      ];
+      # Remove manual phases, rely on hooks
+      unpackPhase = null;
+      buildPhase = null;
+      installPhase = null;
+  });
+}
